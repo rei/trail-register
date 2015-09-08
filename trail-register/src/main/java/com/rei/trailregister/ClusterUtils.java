0b81e689064f014ecf97497f9417db0ff23f6b51
Jeff Skjonsby <jskjons@rei.com>
2015-09-08 15:08:33 -0700

    fixing NPE when env-var is not set

--- trail-register/src/main/java/com/rei/trailregister/ClusterUtils.java
+++ trail-register/src/main/java/com/rei/trailregister/ClusterUtils.java
@@ -2,6 +2,7 @@ package com.rei.trailregister;
 
 import static java.util.stream.Collectors.toList;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -14,10 +15,17 @@ class ClusterUtils {
     
     static List<HostAndPort> parseHostAndPorts(Map<String, String> env) {
         String peersString = env.containsKey(PEERS_SEE_OTHER_VAR) ? env.get(env.get(PEERS_SEE_OTHER_VAR)) : env.get(PEERS_VAR);
+        if (peersString == null) {
+            return Collections.emptyList();
+        }
         return Stream.of(peersString.split(",")).map(ClusterUtils::parseHostAndPort).collect(toList());
     }
 
     private static HostAndPort parseHostAndPort(String input) {
+        if (input == null) {
+            return null;
+        }
+        
         if (input.contains("[") && input.endsWith("]")) { // try jgroups format
             String host = input.substring(0, input.indexOf('['));
             int port = Integer.parseInt(input.substring(input.indexOf('[')+1, input.indexOf(']')));
